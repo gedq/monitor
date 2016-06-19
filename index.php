5cce9f55601ea466684015f6962323dbc58db571
= <gedq@yahoo.com>
2016-06-19 23:40:01 +0100

    index file pushed

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>screen</title>
+<link rel="stylesheet" href="greenmonitor.css">
+</head>
+<body>
+
+
+
+<form>
+<textarea rows="10" cols="60" id="greenmonitor">text here</textarea>
+</form>
+
+<script src="greenmonitor.js"></script>
+</body>
+</html>
